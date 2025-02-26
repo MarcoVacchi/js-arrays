4d30730981f7fb174c4a5eaa07ca78a815b3fd16
MarcoVacchi <mvacchi816@gmail.com>
2025-02-26 14:16:54 +0100

    feat: complete fourth ex

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -15,17 +15,20 @@ const fourthTeacher = teachers [3];
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 teachers [4] = 'Patrick';
-console.log(teachers);
+//console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = teachers.pop();
-console.log (teachers);
-console.log(lastTeacher);
+//console.log (teachers);
+//console.log(lastTeacher);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers.shift();
+console.log(teachers)
+console.log(firstTeacher);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
